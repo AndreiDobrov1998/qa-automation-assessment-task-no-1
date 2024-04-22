57f32e3ce9104729be2f135a90d11561174816dc
Andrei Dobrov <adobrov@squareup.com>
2024-04-22 22:48:48 +0300

    add CI integration 3

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 50 : 2,
+  workers: process.env.CI ? 3 : 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['dot'],['json', { outputFile: 'test-results.json' }]], 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
